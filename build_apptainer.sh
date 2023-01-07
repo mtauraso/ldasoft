47374ff1460fbb64c0b088c2df982f6e0867a38f
Michael Tauraso <mtauraso@uw.edu>
2023-01-06 20:45:58 -0800

    WIP Apptainer definition and build script for RockyLinux. Not tested beyond build stage.

---
+++ build_apptainer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
+#TODO  xcxc take the build prefix from command line like install.sh does
+
+# TODO, if you don't get a build directory from the command line `mktemp` one, 
+# output it, and delete on success pulling the sif file back to wherever we are
+BUILD_PREFIX=/tmp
+
+BUILD_DIR=$BUILD_PREFIX/ldasoft_apptainer
+
+# Note, this script only works when 
+# invoked from the source directory 
+# TODO fix so it uses its own directory rather than pwd
+export SOURCE_DIR=$(pwd)
+
+mkdir -p $BUILD_DIR
+# TODO find and bind the directory we need for openmpi
+apptainer build $BUILD_DIR/ldasoft_apptainer.sif $SOURCE_DIR/apptainer.def
+
+# For now copy back to source dir on success.
+mv $BUILD_DIR/ldasoft_apptainer.sif $SOURCE_DIR
+
+
+# Ideally on success we rm the build dir?
+rm -r $BUILD_DIR
